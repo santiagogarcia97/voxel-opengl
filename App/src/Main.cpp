e0a4a4bb60c6753c624061004a927efd23d8cbce
Santiago Garcia <santiagogarcia97@outlook.com>
2020-07-18 13:22:21 -0300

    index drawing

--- App/src/Main.cpp
+++ App/src/Main.cpp
@@ -5,7 +5,6 @@
 #include "Context.h"
 #include "shaders/ShaderLoader.h"
 
-// Function prototypes
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
 bool wireframe = false;
@@ -15,34 +14,43 @@ int main()
     Context ctx;
     glfwSetKeyCallback(ctx.getWindow(), key_callback);
 
-    // build and compile our shader program
-    // ------------------------------------
     ShaderLoader TriangleShader("src/shaders/Triangle.vert", "src/shaders/Triangle.frag");
 
-    // set up vertex data (and buffer(s)) and configure vertex attributes
-    // ------------------------------------------------------------------
+
     float vertexPositions[] = {
-        // first triangle
-        -0.9f, -0.5f, 0.0f,  1.0f,  0.0f, 0.0f,  // left 
-        -0.0f, -0.5f, 0.0f,  0.0f,  1.0f, 0.0f,  // right
-        -0.45f, 0.5f, 0.0f,  0.0f,  0.0f, 1.0f,  // top 
-        // second triangle
-         0.0f, -0.5f, 0.0f, 0.0f,  0.0f, 1.0f,  // left
-         0.9f, -0.5f, 0.0f, 1.0f,  0.0f, 0.0f,  // right
-         0.45f, 0.5f, 0.0f, 0.0f,  1.0f, 0.0f   // top 
+        -0.9f, -0.5f, 0.0f,  1.0f,  0.0f, 0.0f,  // bot left 
+        -0.1f, -0.5f, 0.0f,  0.0f,  1.0f, 0.0f,  // bot right
+        -0.9f,  0.5f, 0.0f,  0.0f,  0.0f, 1.0f,  // top left
+        -0.1f,  0.5f, 0.0f,  0.0f,  0.0f, 1.0f,  // top right
+
+         0.9f, -0.5f, 0.0f,  1.0f,  0.0f, 0.0f,  // bot left 
+         0.1f, -0.5f, 0.0f,  0.0f,  1.0f, 0.0f,  // bot right
+         0.9f,  0.5f, 0.0f,  0.0f,  0.0f, 1.0f,  // top left
+         0.1f,  0.5f, 0.0f,  0.0f,  0.0f, 1.0f,  // top right
     };
 
-    unsigned int VBO, VAO;
+    unsigned int indices[] = {  
+    0, 1, 3,   // first triangle
+    3, 2, 0,    // second triangle
+    4, 5, 7,
+    7, 6, 4
+    };
+
+    unsigned int VAO, VBO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glGenBuffers(1, &EBO);
+
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 *sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     // color attribute
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
@@ -58,7 +66,6 @@ int main()
 
     // render loop
     // -----------
-
     while (!glfwWindowShouldClose(ctx.getWindow()))
     {
         // Check if any events have been activated (key pressed, mouse moved etc.) and call corresponding response functions
@@ -69,11 +76,10 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // draw our first triangle
+
         TriangleShader.use();
-        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        glDrawArrays(GL_TRIANGLES, 0, 6);
-        // glBindVertexArray(0); // no need to unbind it every time 
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
 
         // Swap the screen buffers
         glfwSwapBuffers(ctx.getWindow());
@@ -81,6 +87,7 @@ int main()
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     // Terminates GLFW, clearing any resources allocated by GLFW.
     glfwTerminate();
     return 0;
